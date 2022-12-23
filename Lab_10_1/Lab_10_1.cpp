46dcd875865d1acbeffbb402a2c0977e44f8f1be
Glit <vpad.lazar.04@gmail.com>
2022-12-23 13:13:55 +0200

    Add project files.

---
+++ Lab_10_1/Lab_10_1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void CreateTXT(char* fname)
+{
+	ofstream fout(fname);
+	char ch;
+	string s;
+	do
+	{
+		cin.get();
+		cin.sync();
+		cout << "enter line: "; getline(cin, s);
+		fout << s << endl;
+		cout << "continue? (y/n): "; cin >> ch;
+	} while (ch == 'y' || ch == 'Y');
+	cout << endl;
+}
+
+string ProcessTXT(char* fname)
+{
+	ifstream fin(fname);
+	string s;
+	while (getline(fin, s))
+	{
+		for (int i = 0; i < s.length() - 2; i++)
+			if (((s[i] == 'O' && s[i + 2] == 'O') || (s[i] == 'A' && s[i + 2] == 'A')) && s[i + 1] == 'G')
+				return "yes";
+	}
+	return "no";
+}
+
+int main()
+{
+	char fname[100];
+	cout << "enter file name : "; cin >> fname;
+	CreateTXT(fname);
+	cout << "Result : " << ProcessTXT(fname) << endl;
+	return 0;
+}
---
+++ UnitTest_Lab_10_1/UnitTest_Lab_10_1.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_10_1/Lab_10_1.cpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab101
+{
+	TEST_CLASS(UnitTestLab101)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			/* Я створив два текстові документи під назвою "UnitTest1.txt" і "UnitTest2.txt",
+			в яких містяться рядки "TESTOGOTESTAGA" і "TESTTEST" відповідно.
+			За допомогою цих документів я перевіряю функцію "ProcessTXT" на роботоспроможність і безпомилковість. */
+			char fname1[255] = "C:/Users/Наталія/source/repos/Lab_10_1/UnitTest_Lab_10_1/UnitTest1.txt", fname2[255] = "C:/Users/Наталія/source/repos/Lab_10_1/UnitTest_Lab_10_1/UnitTest2.txt";
+			string yes = ProcessTXT(fname1); string yes1 = "yes";
+			string no = ProcessTXT(fname2); string no1 = "no";
+			Assert::AreEqual(yes, yes1); 
+			Assert::AreEqual(no, no1);
+		}
+	};
+}
